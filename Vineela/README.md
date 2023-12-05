d0b4770e1a5db4d21cd77577da8fe778381da4db
velpuri vineela <vineelavelpuri@gmail.com>
2023-12-05 16:23:13 +0530

    change supdated

---
+++ Vineela/README.md
@@ -0,0 +1,65 @@
+# Typing Speed Game
+
+Test and improve your typing skills with this interactive Typing Speed Game! Compete against time, track your words per minute (WPM), and challenge yourself to type accurately. The game provides a variety of engaging paragraphs for you to type, with real-time feedback on your performance.
+
+![Game Demo]
+
+
+ 
+## Table of Contents
+- [Introduction](#introduction)
+- [Features](#features)
+- [Usage](#usage)
+- [Project Structure](#project-structure)
+- [Themes](#themes)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+The Typing Speed Game is a web-based application that allows users to practice and enhance their typing abilities. It includes dynamically generated paragraphs for users to type, measures their typing speed, and provides feedback on accuracy.
+
+## Features
+
+- **Randomly generated paragraphs**: Practice typing with a variety of engaging content.
+- **Real-time feedback**: Get instant feedback on words per minute (WPM), mistakes, and characters per minute (CPM).
+- **User-friendly interface**: Enjoy a responsive design and a visually appealing experience.
+- **Theme switcher**: Personalize your experience with a light or dark theme.
+- **Reset button**: Start a new typing session with a single click.
+
+## Usage
+
+1. Open the [live demo](#[(https://velpurivineela.github.io/Typing-Speed-Test-Game-/)]) in a web browser.
+2. Click on the "Start" button to generate a new paragraph.
+3. Type the displayed paragraph in the input field.
+4. Receive real-time feedback on WPM, mistakes, and CPM.
+5. Use the theme switcher to toggle between light and dark themes.
+6. Click the "Reset" button to start a new typing session.
+
+## Project Structure
+
+- **index.html**: The main HTML file with the structure of the web page.
+- **style.css**: The CSS file containing styles for the game's visual elements.
+- **script.js**: The JavaScript file handling game logic, including paragraph generation, typing evaluation, and timer functionality.
+
+## Themes
+
+The game features a theme switcher allowing users to toggle between a light and dark theme. Customize the game's appearance to suit your preferences.
+
+Themes are defined in the CSS file using CSS variables.
+
+## Contributing
+
+Contributions are welcome! If you would like to contribute to the Typing Speed Game, follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and ensure all tests pass.
+4. Create a pull request with a clear description of your changes.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use and modify the code according to your needs.
+
+
+
---
+++ Vineela/script.js
@@ -0,0 +1,148 @@
+const paragraphs = [
+    "Climate is the average weather in a given area over a longer period of time. A description of a climate includes information on, e.g. the average temperature in different seasons, rainfall, and sunshine. Also a description of the (chance of) extremes is often included. Climate change is any systematic change in the long-term statistics of climate variables such as temperature, precipitation, pressure, or wind sustained over several decades or longer.",
+
+    "In the quiet village nestled between rolling hills and meandering streams, time seemed to move at its own pace. The narrow cobblestone streets were lined with charming cottages adorned with colorful flowers, creating a picturesque scene straight out of a storybook. As the sun dipped below the horizon, casting a warm golden glow, the villagers gathered in the town square, sharing laughter and stories that echoed through the peaceful evening air.",
+
+    "The old bookstore stood as a time capsule, its shelves filled with dusty volumes that whispered the tales of bygone eras. As sunlight streamed through the cracked windows, illuminating the dancing particles of dust, the scent of aged paper and ink hung in the air. Each book seemed to carry the weight of countless readers who had wandered through the aisles in search of knowledge, solace, or escape.",
+
+    "High above the city skyline, the skyscrapers stood like colossal sentinels, their glass facades reflecting the ever-changing hues of the sunset. Down below, the hustle and bustle of urban life unfolded, with people rushing to and fro, lost in the rhythm of their daily routines. Amidst the concrete jungle, pockets of greenery offered a refuge, where nature defiantly persisted, a testament to the delicate balance between human progress and the resilience of the natural world.",
+
+    "The aroma of freshly brewed coffee wafted through the air, creating a comforting ambiance in the bustling café. Soft jazz melodies played in the background, harmonizing with the murmur of conversations and the clinking of porcelain cups. Baristas expertly crafted intricate latte art, turning each cup into a canvas of frothy masterpieces. In the cozy corner, a group of friends engaged in animated discussions, their laughter blending with the rich aroma of coffee, creating a haven for connection and camaraderie."
+]
+
+const pg = document.getElementById('pg');
+
+const userinput = document.querySelector('.textinput');
+const resetbtn = document.querySelector('.containerin button'); 
+const totaltime = document.querySelector('.time .txt2');
+const totalwpm = document.querySelector('.wpm .txt2');
+const totalmistake = document.querySelector('.mistake .txt2');
+const totalcpm = document.querySelector('.cpm .txt2');
+
+let timer;
+let maxTime = 120; 
+let timeRemaining = maxTime;
+let charIndex = 0;
+let mistakes = 0;
+let isTyping = 0;
+
+const setParagraph = () => {
+    const randIndex = Math.floor(Math.random()*paragraphs.length);
+    pg.innerText = "";
+    paragraphs[randIndex].split("").forEach(char => {
+        // console.log(char);
+        pg.innerHTML += `<span>${char}</span>`
+    })
+
+    pg.querySelectorAll('span')[0].classList.add('active');
+    document.addEventListener("keydown", () => userinput.focus())
+    pg.addEventListener("click", () => userinput.focus())
+    
+    totaltime.innerText = timeRemaining;
+    totalwpm.innerText = 0;
+    totalmistake.innerText = 0;
+    totalcpm.innerText = 0;
+
+}
+const startTyping = () => {
+    let characters = pg.querySelectorAll('span');
+    console.log(characters);
+    let typedChar = userinput.value.split("")[charIndex];
+    if(charIndex < characters.length - 1 && timeRemaining > 0){
+      if(!isTyping){
+        // 0 or false
+        timer = setInterval(startTimer, 1000);
+        isTyping = true;
+      }
+
+      if(typedChar == null){
+        if(charIndex > 0){
+           charIndex--;
+           if(characters[charIndex].classList.contains("incorrect")){
+               mistakes--;
+           }
+           characters[charIndex].classList.remove("incorrect", "correct");
+        }
+      }
+      else{
+       if(characters[charIndex].innerText == typedChar){
+        characters[charIndex].classList.add("correct");
+
+       }
+       else {
+        characters[charIndex].classList.add("incorrect");
+        mistakes++;
+       }
+       charIndex++; 
+      }
+
+      characters.forEach(char => {
+        char.classList.remove("active");
+      })
+      characters[charIndex].classList.add("active");
+
+      let wpm = Math.round(((charIndex-mistakes)/5) / (maxTime-timeRemaining)*60)
+       wpm = wpm < 0 || !wpm || wpm == Infinity ? 0 : wpm;
+       totalwpm.innerText = wpm;
+       totalmistake.innerText = mistakes;
+       totalcpm.innerText = charIndex - mistakes;
+
+    }
+
+    else{
+        clearInterval(timer);
+        isTyping = false;
+    }
+    
+}
+const startTimer = () => {
+    if(timeRemaining > 0){
+        timeRemaining--;
+        totaltime.innerText = timeRemaining;
+        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeRemaining) * 60)
+        totalwpm.innerHTML = wpm;
+    }
+    else {
+        clearInterval(timer);
+        isTyping = false;
+    }
+}
+
+const resetGame = () => {
+    setParagraph();
+    clearInterval(timer);
+    timeRemaining = maxTime;
+    charIndex = 0;
+    mistakes = 0;
+    isTyping = 0;
+    userinput.value = "";
+    totaltime.innerText = timeRemaining;
+    totalwpm.innerText = 0;
+    totalmistake.innerText = 0;
+    totalcpm.innerText = 0;
+}
+
+setParagraph();
+resetbtn.addEventListener('click', resetGame);
+userinput.addEventListener('input', startTyping);
+
+
+// Switch function
+
+const switchTheme = () => {
+    // Get the root element and data-theme value
+
+    const rootElem = document.documentElement;
+    let dataTheme = rootElem.getAttribute('data-theme'), newTheme;
+
+    newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+    // Set the new HTML attribute
+
+    rootElem.setAttribute('data-theme', newTheme);
+
+}
+
+// Add event listner for the theme switcher
+
+document.querySelector('#theme-switcher').addEventListener('click', switchTheme)
