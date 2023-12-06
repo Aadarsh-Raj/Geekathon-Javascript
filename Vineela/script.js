fda5c6aa13994b2755da55e8117c5c2f9be7e187
velpuri vineela <vineelavelpuri@gmail.com>
2023-12-06 11:23:48 +0530

    Comments added

--- Vineela/script.js
+++ Vineela/script.js
@@ -1,3 +1,4 @@
+// Array of paragraphs for the typing test
 const paragraphs = [
     "Climate is the average weather in a given area over a longer period of time. A description of a climate includes information on, e.g. the average temperature in different seasons, rainfall, and sunshine. Also a description of the (chance of) extremes is often included. Climate change is any systematic change in the long-term statistics of climate variables such as temperature, precipitation, pressure, or wind sustained over several decades or longer.",
 
@@ -10,8 +11,9 @@ const paragraphs = [
     "The aroma of freshly brewed coffee wafted through the air, creating a comforting ambiance in the bustling café. Soft jazz melodies played in the background, harmonizing with the murmur of conversations and the clinking of porcelain cups. Baristas expertly crafted intricate latte art, turning each cup into a canvas of frothy masterpieces. In the cozy corner, a group of friends engaged in animated discussions, their laughter blending with the rich aroma of coffee, creating a haven for connection and camaraderie."
 ]
 
-const pg = document.getElementById('pg');
 
+// Elements from the HTML document
+const pg = document.getElementById('pg');
 const userinput = document.querySelector('.textinput');
 const resetbtn = document.querySelector('.containerin button'); 
 const totaltime = document.querySelector('.time .txt2');
@@ -19,6 +21,8 @@ const totalwpm = document.querySelector('.wpm .txt2');
 const totalmistake = document.querySelector('.mistake .txt2');
 const totalcpm = document.querySelector('.cpm .txt2');
 
+
+// Timer and typing variables
 let timer;
 let maxTime = 120; 
 let timeRemaining = maxTime;
@@ -26,61 +30,94 @@ let charIndex = 0;
 let mistakes = 0;
 let isTyping = 0;
 
+
+// Function to set a random paragraph for typing
 const setParagraph = () => {
+    // Select a random paragraph from the array
     const randIndex = Math.floor(Math.random()*paragraphs.length);
+    
+    // Clear the existing content of the paragraph element
     pg.innerText = "";
+    
+    // Split the paragraph into individual characters and create spans
     paragraphs[randIndex].split("").forEach(char => {
         // console.log(char);
         pg.innerHTML += `<span>${char}</span>`
     })
 
+    // Highlight the first character and set up event listeners
     pg.querySelectorAll('span')[0].classList.add('active');
     document.addEventListener("keydown", () => userinput.focus())
     pg.addEventListener("click", () => userinput.focus())
     
+
+    // Initialize and display initial statistics
     totaltime.innerText = timeRemaining;
     totalwpm.innerText = 0;
     totalmistake.innerText = 0;
     totalcpm.innerText = 0;
 
 }
+
+// Function to start the typing test
 const startTyping = () => {
+    // Get all characters from the paragraph
     let characters = pg.querySelectorAll('span');
-    console.log(characters);
+    // console.log(characters);
+
+    // Get the currently typed character
     let typedChar = userinput.value.split("")[charIndex];
+
+    // Check if there are characters left to type and time is remaining
     if(charIndex < characters.length - 1 && timeRemaining > 0){
+       
+      // Start the timer if not already started
       if(!isTyping){
         // 0 or false
         timer = setInterval(startTimer, 1000);
         isTyping = true;
       }
+      
 
+      // Check if the typed character is null
       if(typedChar == null){
         if(charIndex > 0){
+            // Move back if there is a previous character
            charIndex--;
+
+           // Adjust mistakes count if the previous character was a null
            if(characters[charIndex].classList.contains("incorrect")){
                mistakes--;
            }
+
+           // Remove styling for incorrect and correct
            characters[charIndex].classList.remove("incorrect", "correct");
         }
       }
       else{
+       // Check if the typed character is correct 
        if(characters[charIndex].innerText == typedChar){
         characters[charIndex].classList.add("correct");
 
        }
        else {
+        // Handle incorrect character
         characters[charIndex].classList.add("incorrect");
         mistakes++;
        }
+
+       // Move to the next character
        charIndex++; 
       }
+      
 
+      // Update styles for active character
       characters.forEach(char => {
         char.classList.remove("active");
       })
       characters[charIndex].classList.add("active");
-
+      
+      // Calculate and display WPM, Mistakes, and CPM
       let wpm = Math.round(((charIndex-mistakes)/5) / (maxTime-timeRemaining)*60)
        wpm = wpm < 0 || !wpm || wpm == Infinity ? 0 : wpm;
        totalwpm.innerText = wpm;
@@ -90,31 +127,46 @@ const startTyping = () => {
     }
 
     else{
+        // Stop the timer if all characters are typed or time is up
         clearInterval(timer);
         isTyping = false;
     }
     
 }
+
+// Function to update the timer while typing
 const startTimer = () => {
     if(timeRemaining > 0){
+        // Update time remaining and display it
         timeRemaining--;
         totaltime.innerText = timeRemaining;
+
+        // Calculate and display WPM
         let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeRemaining) * 60)
         totalwpm.innerHTML = wpm;
     }
     else {
+        // Stop the timer if time is up
         clearInterval(timer);
         isTyping = false;
     }
 }
 
+
+// Function to reset the game
 const resetGame = () => {
+    // Set a new random paragraph
     setParagraph();
+
+
+    // Clear the timer and reset variables
     clearInterval(timer);
     timeRemaining = maxTime;
     charIndex = 0;
     mistakes = 0;
     isTyping = 0;
+
+    // Clear user input and reset statistics
     userinput.value = "";
     totaltime.innerText = timeRemaining;
     totalwpm.innerText = 0;
@@ -122,7 +174,10 @@ const resetGame = () => {
     totalcpm.innerText = 0;
 }
 
+// Initial setup for the typing test
 setParagraph();
+
+// Event listeners for the reset button and user input
 resetbtn.addEventListener('click', resetGame);
 userinput.addEventListener('input', startTyping);
 
